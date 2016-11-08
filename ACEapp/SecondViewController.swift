fc954a8aaa9e6f35619e0fcf8dd4449b1ce85ac5
Madelyn Nelson <mnelso12@nd.edu>
2016-11-08 15:38:54 -0500

    Initial Commit

---
+++ ACEapp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ACEapp
+//
+//  Created by Madelyn Nelson on 11/8/16.
+//  Copyright © 2016 Madelyn Nelson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
